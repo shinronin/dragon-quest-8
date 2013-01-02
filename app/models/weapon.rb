9383e43e932d1da2b84fe6fcafbf264e5d3f73cc
shinronin <doug.riordan@gmail.com>
2013-01-01 21:44:58 -0500

    fixed bugs found via db:reset

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,5 +1,6 @@
 class Weapon < ActiveRecord::Base
-  attr_accessible :bonus, :cost, :name, :other_bonus, :shop_location, :type, :value
+  attr_accessible :alchemy, :bonus, :buy, :cost, :kind, :name, :other_bonus,
+  :shop_location, :treasure, :value
   has_many :wielders
   has_many :characters, :through => :wielders
 end
